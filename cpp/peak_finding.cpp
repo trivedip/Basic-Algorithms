17c39a74a2cf78a36021247769facd7a83a46c24
priyank <pri7ank@gmail.com>
2019-12-29 15:44:05 -0600

    Bug fix

---
+++ cpp/peak_finding.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Priyank on 12/27/2019.
+//
+
+#include <cmath>
+#include <iostream>
+#ifndef BASIC_ALGORITHMS_CPP_PEAK_FINDING_H
+#include "peak_finding.h"
+#endif
+enum Apple : unsigned char
+{A=0, B, C};
+void peak_finder::findLocalPeak(int arr[], int size) {
+    int mid = ceil(size/2);
+    Apple val = B;
+    std::cout << val << std::endl;
+    for(size_t i=0; i< size; i++){
+
+
+    }
+}
