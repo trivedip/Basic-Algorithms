1538b7636101f89e72074982fd07f7e0c7a4e17b
priyank <pri7ank@gmail.com>
2020-01-05 17:52:17 -0600

    added local peak finder with std::array, contains bug.

--- cpp/driver.cpp
+++ cpp/driver.cpp
@@ -21,45 +21,62 @@
 using namespace std;
 using namespace util;
 
-
+void arraCall(int *arr_one,int size){
+    std::cout<< &(arr_one[2]) << std::endl;
+    std::cout<< (arr_one[2]) << std::endl;
+}
 int main()
 {
     int arr[10];
-    auto main_time = chrono::high_resolution_clock::now();
 
+//    auto main_time = chrono::high_resolution_clock::now();
+    int* test_arr = new int[10];
 #if 0  //If block 1 - related to exp.h and exp.cpp
+    template_exp<int,10> exp(10);
+    exp.print_val();
+
 //    Good example of inheritence and how object creation and memory allotion on heap works.
-    Exp exp;
+//    Exp exp;
     Exp* exp_one = new Exp;     // Equivilent to Exp* exp_one = (Exp*)malloc(sizeof(Entity));    // (*Exp).print()
     (*exp_one).print();
     std::shared_ptr<Exp> exp2 = std::make_shared<Exp>();
     exp2->print();
     Exp_extend expt;
-    exp.print();
+//    exp.print();
     expt.print();
-#endif
+
     vector<int> a;
     vector<int> b;
     Utility util;
-    peak_finder p;
+
     Sort srt;
-
     util.fillArray(arr,10);
-    p.findLocalPeak(arr, 10);
-
-    util.tStart();
+#endif
+    std::array<int,5> something = {1,2,5,3,5};
+    peak_finder p;
+//    int arr_one[5] = {0,5,4,7,0};
+    std::cout << p.findLocalPeak(something,0,0) << std::endl;
+//    arraCall(&arr_one[1], 4);
 #if 0
+    util.tStart();
+
+
     util.fillVector(a,100);
     b = a;
     srt.BubbleSort(a);
     util.printVector2(b,a);
     util.tEnd(__FUNCTION__);
 
-
     util.pStat();
     auto main_time_end = chrono::high_resolution_clock::now();
     auto main_elapsed = std::chrono::duration_cast<std::chrono::microseconds>(main_time_end - main_time);
     cout  << "Time calculate from main function: " << main_elapsed.count() << endl;
+
+
+
+
+
+
 #endif
     return 0;
 }
