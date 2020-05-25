33177e662686a36a6df6d73c8f0947b56ccf4440
priyank <pri7ank@gmail.com>
2020-05-24 19:30:34 -0500

    Maintenance

--- python/driver.py
+++ python/driver.py
@@ -1,8 +1,17 @@
 import random
-from tree import LinkedList
+from linkedlist import LinkedList
+from tree import BST
 
 a = LinkedList(5)
 for _ in range(10):
     a.append(random.randint(0,50))
 
-a.print()
+# a.print()
+
+b = BST(5)
+
+b.append(10)
+for _ in range(5):
+    b.append(random.randint(0,100))
+b.pr()
+
--- python/tree.py
+++ python/tree.py
@@ -1,25 +1,52 @@
 class Node:
+    # Node class for creating tree nodes, has value to store data and left and right to direct to childrens
     def __init__(self,val):
         self.value = val
-        self.next = None
+        self.left = None
+        self.right = None
 
-class LinkedList:
+class BST:
+    # Class to create binery search tree. 
+    root = None
     def __init__(self,val):
-        self.head = Node(val)
+        if not self.root:
+            self.root = Node(val)
+        else: self.append(val)
     
     def append(self,val):
-        temo = self.head
-        while self.head.next:
-            self.head = self.head.next
-        
-        self.head.next = Node(val)
-        self.head = temo
+        self._append(self.root, val)    
     
-    def print(self):
-        temo = self.head
-        while temo:
-            print(temo.value)
-            temo = temo.next
+    def _append(self, node, val):
+        if not node:
+            return False
+        
+        if (val < node.value):
+            if not node.left:
+                node.left = Node(val)
+                return True
+            else:
+                self._append(node.left, val)
+        else:
+            if not node.right:
+                node.right = Node(val)
+                return True
+            else:
+                self._append(node.right,val)
 
-if __name__ == "__main__":
-    print("This file is supposed to be called from driver.py file.")
+    def pr(self):
+        temp_root = self.root
+        self._print(self.root)
+        self.root = temp_root
+
+    def _print(self, temp_node):
+        if not temp_node:
+            return False
+        
+        else:
+            if(temp_node.left):
+                self._print(temp_node.left)
+            print(temp_node.value)
+            if(temp_node.right):
+                self._print(temp_node.right)
+
+    
